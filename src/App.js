3af8f48d9cd9f797256ef5817cb1c67be51ccea4
Ernesto Caceres <ernesto.caceresay@gmail.com>
2021-04-27 01:03:23 -0500

    Primer commit ( plantilla basica )
    En este commit tengo la plantilla basica de react pero solo probando funciones basicas de GSAP, posteriormente organizaré en secciones o en paginas
    con react-router para en cada componente ir tomando mi curso completo de GSAP

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { gsap } from "gsap";
+
+import logo from "./img/logo.png";
+import football from "./img/img.png";
+import table from "./img/table.png";
 
 function App() {
+      
+useEffect(() => {
+    
+	gsap.to(".logo", {
+      duration: 2,
+      x: 300,
+      backgroundColor: "#560563",
+      borderRadius: "20%",
+      border: "5px solid white",
+      ease: "elastic",
+    });
+	
+	gsap.set(".logo2, .table", {transformOrigin: "50% 50%"});
+	gsap.to(".logo2, .table", {duration: 2, rotation: 360});
+
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="demo">
+        <img src={logo} alt="" className="logo" width="400px" />
+      </div>
+
+      <div>
+        <img className="logo2" src={football} alt="" width="400px" /> <br/>
+        <img className="table" src={table} alt="table" width="400px"/>
+      </div>
+    </>
   );
 }
 
